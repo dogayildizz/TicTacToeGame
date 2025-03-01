ccf33724d6c52888fdb95119a7e7198fb8d098ca
Doğa Yıldız <dogasuyildizz@gmail.com>
2025-03-01 22:09:53 +0300

    Add project files.

---
+++ WFAXOXOyunu.UI/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WFAXOXOyunu.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            btnYeniOyun = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btn11 = new Button();
+            btn12 = new Button();
+            btn13 = new Button();
+            btn23 = new Button();
+            btn22 = new Button();
+            btn21 = new Button();
+            btn31 = new Button();
+            btn32 = new Button();
+            btn33 = new Button();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._128_1288069_grid_background_png;
+            pictureBox1.Location = new Point(19, 116);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(495, 496);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnYeniOyun
+            // 
+            btnYeniOyun.Location = new Point(328, 684);
+            btnYeniOyun.Name = "btnYeniOyun";
+            btnYeniOyun.Size = new Size(184, 58);
+            btnYeniOyun.TabIndex = 1;
+            btnYeniOyun.Text = "Yeni Oyun";
+            btnYeniOyun.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Underline);
+            label1.Location = new Point(19, 663);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 28);
+            label1.TabIndex = 2;
+            label1.Text = "Skor Tablosu";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.Location = new Point(19, 704);
+            label2.Name = "label2";
+            label2.Size = new Size(172, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Birinci Oyuncu (X) :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F);
+            label3.Location = new Point(19, 732);
+            label3.Name = "label3";
+            label3.Size = new Size(166, 25);
+            label3.TabIndex = 3;
+            label3.Text = "İkinci Oyuncu (O) :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Algerian", 40F);
+            label4.Location = new Point(65, 14);
+            label4.Name = "label4";
+            label4.Size = new Size(416, 75);
+            label4.TabIndex = 4;
+            label4.Text = "Tic Tac Toe";
+            // 
+            // btn11
+            // 
+            btn11.FlatStyle = FlatStyle.Flat;
+            btn11.ForeColor = SystemColors.ActiveCaption;
+            btn11.Location = new Point(22, 120);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(157, 155);
+            btn11.TabIndex = 5;
+            btn11.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            btn12.FlatStyle = FlatStyle.Flat;
+            btn12.ForeColor = SystemColors.ActiveCaption;
+            btn12.Location = new Point(189, 120);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(157, 155);
+            btn12.TabIndex = 5;
+            btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn13
+            // 
+            btn13.FlatStyle = FlatStyle.Flat;
+            btn13.ForeColor = SystemColors.ActiveCaption;
+            btn13.Location = new Point(355, 120);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(157, 155);
+            btn13.TabIndex = 5;
+            btn13.UseVisualStyleBackColor = true;
+            // 
+            // btn23
+            // 
+            btn23.FlatStyle = FlatStyle.Flat;
+            btn23.ForeColor = SystemColors.ActiveCaption;
+            btn23.Location = new Point(355, 285);
+            btn23.Name = "btn23";
+            btn23.Size = new Size(157, 155);
+            btn23.TabIndex = 5;
+            btn23.UseVisualStyleBackColor = true;
+            // 
+            // btn22
+            // 
+            btn22.FlatStyle = FlatStyle.Flat;
+            btn22.ForeColor = SystemColors.ActiveCaption;
+            btn22.Location = new Point(189, 285);
+            btn22.Name = "btn22";
+            btn22.Size = new Size(157, 155);
+            btn22.TabIndex = 5;
+            btn22.UseVisualStyleBackColor = true;
+            // 
+            // btn21
+            // 
+            btn21.FlatStyle = FlatStyle.Flat;
+            btn21.ForeColor = SystemColors.ActiveCaption;
+            btn21.Location = new Point(22, 285);
+            btn21.Name = "btn21";
+            btn21.Size = new Size(157, 155);
+            btn21.TabIndex = 5;
+            btn21.UseVisualStyleBackColor = true;
+            // 
+            // btn31
+            // 
+            btn31.FlatStyle = FlatStyle.Flat;
+            btn31.ForeColor = SystemColors.ActiveCaption;
+            btn31.Location = new Point(22, 451);
+            btn31.Name = "btn31";
+            btn31.Size = new Size(157, 155);
+            btn31.TabIndex = 5;
+            btn31.UseVisualStyleBackColor = true;
+            // 
+            // btn32
+            // 
+            btn32.FlatStyle = FlatStyle.Flat;
+            btn32.ForeColor = SystemColors.ActiveCaption;
+            btn32.Location = new Point(189, 451);
+            btn32.Name = "btn32";
+            btn32.Size = new Size(157, 155);
+            btn32.TabIndex = 5;
+            btn32.UseVisualStyleBackColor = true;
+            // 
+            // btn33
+            // 
+            btn33.FlatStyle = FlatStyle.Flat;
+            btn33.ForeColor = SystemColors.ActiveCaption;
+            btn33.Location = new Point(357, 451);
+            btn33.Name = "btn33";
+            btn33.Size = new Size(157, 155);
+            btn33.TabIndex = 5;
+            btn33.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Underline);
+            label5.Location = new Point(19, 635);
+            label5.Name = "label5";
+            label5.Size = new Size(137, 28);
+            label5.TabIndex = 2;
+            label5.Text = "Oyun sayısı : ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(534, 784);
+            Controls.Add(btn33);
+            Controls.Add(btn32);
+            Controls.Add(btn31);
+            Controls.Add(btn21);
+            Controls.Add(btn22);
+            Controls.Add(btn23);
+            Controls.Add(btn13);
+            Controls.Add(btn12);
+            Controls.Add(btn11);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(label1);
+            Controls.Add(btnYeniOyun);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnYeniOyun;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button btn11;
+        private Button btn12;
+        private Button btn13;
+        private Button btn23;
+        private Button btn22;
+        private Button btn21;
+        private Button btn31;
+        private Button btn32;
+        private Button btn33;
+        private Label label5;
+    }
+}
