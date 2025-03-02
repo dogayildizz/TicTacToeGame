d4f9b97f83bf1e9526ab2febd7ee144e18405816
Doğa Yıldız <dogasuyildizz@gmail.com>
2025-03-02 18:12:14 +0300

    Added the functionality for the XOX game; the game is working now. Detailed error handling and design improvements will be done later. The project is still not finished.

--- WFAXOXOyunu.UI/Form1.Designer.cs
+++ WFAXOXOyunu.UI/Form1.Designer.cs
@@ -34,16 +34,18 @@
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
-            btn11 = new Button();
-            btn12 = new Button();
-            btn13 = new Button();
-            btn23 = new Button();
-            btn22 = new Button();
-            btn21 = new Button();
-            btn31 = new Button();
-            btn32 = new Button();
-            btn33 = new Button();
-            label5 = new Label();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            txtBilgilendirme = new TextBox();
+            lblBirinciOyuncuKazanmaSayisi = new Label();
+            lblIkinciOyuncuKazanmaSayisi = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -59,18 +61,19 @@
             // 
             // btnYeniOyun
             // 
-            btnYeniOyun.Location = new Point(328, 684);
+            btnYeniOyun.Location = new Point(531, 548);
             btnYeniOyun.Name = "btnYeniOyun";
-            btnYeniOyun.Size = new Size(184, 58);
+            btnYeniOyun.Size = new Size(227, 58);
             btnYeniOyun.TabIndex = 1;
             btnYeniOyun.Text = "Yeni Oyun";
             btnYeniOyun.UseVisualStyleBackColor = true;
+            btnYeniOyun.Click += btnYeniOyun_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Underline);
-            label1.Location = new Point(19, 663);
+            label1.Location = new Point(531, 345);
             label1.Name = "label1";
             label1.Size = new Size(132, 28);
             label1.TabIndex = 2;
@@ -80,7 +83,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 11F);
-            label2.Location = new Point(19, 704);
+            label2.Location = new Point(531, 386);
             label2.Name = "label2";
             label2.Size = new Size(172, 25);
             label2.TabIndex = 3;
@@ -90,7 +93,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 11F);
-            label3.Location = new Point(19, 732);
+            label3.Location = new Point(531, 414);
             label3.Name = "label3";
             label3.Size = new Size(166, 25);
             label3.TabIndex = 3;
@@ -106,125 +109,165 @@
             label4.TabIndex = 4;
             label4.Text = "Tic Tac Toe";
             // 
-            // btn11
+            // btn1
             // 
-            btn11.FlatStyle = FlatStyle.Flat;
-            btn11.ForeColor = SystemColors.ActiveCaption;
-            btn11.Location = new Point(22, 120);
-            btn11.Name = "btn11";
-            btn11.Size = new Size(157, 155);
-            btn11.TabIndex = 5;
-            btn11.UseVisualStyleBackColor = true;
+            btn1.FlatStyle = FlatStyle.Flat;
+            btn1.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn1.ForeColor = SystemColors.ActiveCaption;
+            btn1.Location = new Point(22, 120);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(157, 155);
+            btn1.TabIndex = 5;
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
             // 
-            // btn12
+            // btn2
             // 
-            btn12.FlatStyle = FlatStyle.Flat;
-            btn12.ForeColor = SystemColors.ActiveCaption;
-            btn12.Location = new Point(189, 120);
-            btn12.Name = "btn12";
-            btn12.Size = new Size(157, 155);
-            btn12.TabIndex = 5;
-            btn12.UseVisualStyleBackColor = true;
+            btn2.FlatStyle = FlatStyle.Flat;
+            btn2.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn2.ForeColor = SystemColors.ActiveCaption;
+            btn2.Location = new Point(189, 120);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(157, 155);
+            btn2.TabIndex = 5;
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
             // 
-            // btn13
+            // btn3
             // 
-            btn13.FlatStyle = FlatStyle.Flat;
-            btn13.ForeColor = SystemColors.ActiveCaption;
-            btn13.Location = new Point(355, 120);
-            btn13.Name = "btn13";
-            btn13.Size = new Size(157, 155);
-            btn13.TabIndex = 5;
-            btn13.UseVisualStyleBackColor = true;
+            btn3.FlatStyle = FlatStyle.Flat;
+            btn3.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn3.ForeColor = SystemColors.ActiveCaption;
+            btn3.Location = new Point(355, 120);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(157, 155);
+            btn3.TabIndex = 5;
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
             // 
-            // btn23
+            // btn6
             // 
-            btn23.FlatStyle = FlatStyle.Flat;
-            btn23.ForeColor = SystemColors.ActiveCaption;
-            btn23.Location = new Point(355, 285);
-            btn23.Name = "btn23";
-            btn23.Size = new Size(157, 155);
-            btn23.TabIndex = 5;
-            btn23.UseVisualStyleBackColor = true;
+            btn6.FlatStyle = FlatStyle.Flat;
+            btn6.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn6.ForeColor = SystemColors.ActiveCaption;
+            btn6.Location = new Point(355, 285);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(157, 155);
+            btn6.TabIndex = 5;
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
             // 
-            // btn22
+            // btn5
             // 
-            btn22.FlatStyle = FlatStyle.Flat;
-            btn22.ForeColor = SystemColors.ActiveCaption;
-            btn22.Location = new Point(189, 285);
-            btn22.Name = "btn22";
-            btn22.Size = new Size(157, 155);
-            btn22.TabIndex = 5;
-            btn22.UseVisualStyleBackColor = true;
+            btn5.FlatStyle = FlatStyle.Flat;
+            btn5.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn5.ForeColor = SystemColors.ActiveCaption;
+            btn5.Location = new Point(189, 285);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(157, 155);
+            btn5.TabIndex = 5;
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
             // 
-            // btn21
+            // btn4
             // 
-            btn21.FlatStyle = FlatStyle.Flat;
-            btn21.ForeColor = SystemColors.ActiveCaption;
-            btn21.Location = new Point(22, 285);
-            btn21.Name = "btn21";
-            btn21.Size = new Size(157, 155);
-            btn21.TabIndex = 5;
-            btn21.UseVisualStyleBackColor = true;
+            btn4.FlatStyle = FlatStyle.Flat;
+            btn4.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn4.ForeColor = SystemColors.ActiveCaption;
+            btn4.Location = new Point(22, 285);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(157, 155);
+            btn4.TabIndex = 5;
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
             // 
-            // btn31
+            // btn7
             // 
-            btn31.FlatStyle = FlatStyle.Flat;
-            btn31.ForeColor = SystemColors.ActiveCaption;
-            btn31.Location = new Point(22, 451);
-            btn31.Name = "btn31";
-            btn31.Size = new Size(157, 155);
-            btn31.TabIndex = 5;
-            btn31.UseVisualStyleBackColor = true;
+            btn7.FlatStyle = FlatStyle.Flat;
+            btn7.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn7.ForeColor = SystemColors.ActiveCaption;
+            btn7.Location = new Point(22, 451);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(157, 155);
+            btn7.TabIndex = 5;
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
             // 
-            // btn32
+            // btn8
             // 
-            btn32.FlatStyle = FlatStyle.Flat;
-            btn32.ForeColor = SystemColors.ActiveCaption;
-            btn32.Location = new Point(189, 451);
-            btn32.Name = "btn32";
-            btn32.Size = new Size(157, 155);
-            btn32.TabIndex = 5;
-            btn32.UseVisualStyleBackColor = true;
+            btn8.FlatStyle = FlatStyle.Flat;
+            btn8.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn8.ForeColor = SystemColors.ActiveCaption;
+            btn8.Location = new Point(189, 451);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(157, 155);
+            btn8.TabIndex = 5;
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
             // 
-            // btn33
+            // btn9
             // 
-            btn33.FlatStyle = FlatStyle.Flat;
-            btn33.ForeColor = SystemColors.ActiveCaption;
-            btn33.Location = new Point(357, 451);
-            btn33.Name = "btn33";
-            btn33.Size = new Size(157, 155);
-            btn33.TabIndex = 5;
-            btn33.UseVisualStyleBackColor = true;
+            btn9.FlatStyle = FlatStyle.Flat;
+            btn9.Font = new Font("Segoe UI", 70F, FontStyle.Bold);
+            btn9.ForeColor = SystemColors.ActiveCaption;
+            btn9.Location = new Point(357, 451);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(157, 155);
+            btn9.TabIndex = 5;
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
             // 
-            // label5
+            // txtBilgilendirme
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Underline);
-            label5.Location = new Point(19, 635);
-            label5.Name = "label5";
-            label5.Size = new Size(137, 28);
-            label5.TabIndex = 2;
-            label5.Text = "Oyun sayısı : ";
+            txtBilgilendirme.Font = new Font("Segoe UI", 12F);
+            txtBilgilendirme.Location = new Point(531, 133);
+            txtBilgilendirme.Multiline = true;
+            txtBilgilendirme.Name = "txtBilgilendirme";
+            txtBilgilendirme.ReadOnly = true;
+            txtBilgilendirme.Size = new Size(227, 159);
+            txtBilgilendirme.TabIndex = 6;
+            // 
+            // lblBirinciOyuncuKazanmaSayisi
+            // 
+            lblBirinciOyuncuKazanmaSayisi.AutoSize = true;
+            lblBirinciOyuncuKazanmaSayisi.Font = new Font("Segoe UI", 11F);
+            lblBirinciOyuncuKazanmaSayisi.Location = new Point(695, 386);
+            lblBirinciOyuncuKazanmaSayisi.Name = "lblBirinciOyuncuKazanmaSayisi";
+            lblBirinciOyuncuKazanmaSayisi.Size = new Size(22, 25);
+            lblBirinciOyuncuKazanmaSayisi.TabIndex = 7;
+            lblBirinciOyuncuKazanmaSayisi.Text = "0";
+            // 
+            // lblIkinciOyuncuKazanmaSayisi
+            // 
+            lblIkinciOyuncuKazanmaSayisi.AutoSize = true;
+            lblIkinciOyuncuKazanmaSayisi.Font = new Font("Segoe UI", 11F);
+            lblIkinciOyuncuKazanmaSayisi.Location = new Point(695, 415);
+            lblIkinciOyuncuKazanmaSayisi.Name = "lblIkinciOyuncuKazanmaSayisi";
+            lblIkinciOyuncuKazanmaSayisi.Size = new Size(22, 25);
+            lblIkinciOyuncuKazanmaSayisi.TabIndex = 7;
+            lblIkinciOyuncuKazanmaSayisi.Text = "0";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(534, 784);
-            Controls.Add(btn33);
-            Controls.Add(btn32);
-            Controls.Add(btn31);
-            Controls.Add(btn21);
-            Controls.Add(btn22);
-            Controls.Add(btn23);
-            Controls.Add(btn13);
-            Controls.Add(btn12);
-            Controls.Add(btn11);
+            ClientSize = new Size(770, 626);
+            Controls.Add(lblIkinciOyuncuKazanmaSayisi);
+            Controls.Add(lblBirinciOyuncuKazanmaSayisi);
+            Controls.Add(txtBilgilendirme);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn4);
+            Controls.Add(btn5);
+            Controls.Add(btn6);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(label5);
             Controls.Add(label1);
             Controls.Add(btnYeniOyun);
             Controls.Add(pictureBox1);
@@ -243,15 +286,17 @@
         private Label label2;
         private Label label3;
         private Label label4;
-        private Button btn11;
-        private Button btn12;
-        private Button btn13;
-        private Button btn23;
-        private Button btn22;
-        private Button btn21;
-        private Button btn31;
-        private Button btn32;
-        private Button btn33;
-        private Label label5;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private TextBox txtBilgilendirme;
+        private Label lblBirinciOyuncuKazanmaSayisi;
+        private Label lblIkinciOyuncuKazanmaSayisi;
     }
 }
--- WFAXOXOyunu.UI/Form1.cs
+++ WFAXOXOyunu.UI/Form1.cs
@@ -1,10 +1,161 @@
+using System.Diagnostics.Eventing.Reader;
+
 namespace WFAXOXOyunu.UI
 {
     public partial class Form1 : Form
     {
+        bool birinciOyuncununSirasi = true;
+        int birinciOyuncununKazanmaSayisi = 0, ikinciOyuncununKazanmaSayisi = 0 ,tiklananButonSayisi=0;
+
         public Form1()
         {
-           
+           InitializeComponent();
+           txtBilgilendirme.Text = "Birinci Oyuncunun (X) sýrasý...";
         }
+
+        public void OyunuTemizle()
+        {
+            Button[] butonlar = { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+
+            foreach (Button btn in butonlar)
+            {
+                btn.Text = "";
+                btn.Enabled = true;
+            }
+            txtBilgilendirme.Text = "Birinci Oyuncunun (X) sýrasý...";
+        }
+
+        public void BasilanButon(Button button)
+        {
+            tiklananButonSayisi++;
+            button.Enabled = false;
+            if (birinciOyuncununSirasi)
+            {
+                button.Text = "X";
+                birinciOyuncununSirasi = false;
+                txtBilgilendirme.Text = "Ýkinci Oyuncunun (O) sýrasý...";
+            }
+            else
+            {
+                button.Text = "O";
+                birinciOyuncununSirasi = true;
+                txtBilgilendirme.Text = "Birinci Oyuncunun (X) sýrasý...";
+            }
+            if (KazananVarMi())
+            {
+                tiklananButonSayisi = 0;
+                if (!birinciOyuncununSirasi)
+                {
+                    MessageBox.Show("Tebrikler! Birinci Oyuncu(X) Kazandý. ");
+                    OyunuTemizle();
+                    birinciOyuncununKazanmaSayisi++;
+                    lblBirinciOyuncuKazanmaSayisi.Text = birinciOyuncununKazanmaSayisi.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Tebrikler! Ýkinci Oyuncu(O) Kazandý. ");
+                    OyunuTemizle();
+                    ikinciOyuncununKazanmaSayisi++;
+                    lblIkinciOyuncuKazanmaSayisi.Text = ikinciOyuncununKazanmaSayisi.ToString();
+                }
+            }
+            if(tiklananButonSayisi==9&&!KazananVarMi())
+            {
+                MessageBox.Show("Berabere! Kazanan yok. ");
+                OyunuTemizle();
+                tiklananButonSayisi = 0;
+            }
+        }  
+
+        public bool KazananVarMi()
+        {
+
+            bool kazananVarMi = false;
+            Button[] butonlar = { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            for (int i = 0; i < butonlar.Length; i += 3) //SATIR KONTROLU
+            {
+                if (butonlar[i].Text != "" && butonlar[i].Text == butonlar[i + 1].Text && butonlar[i].Text == butonlar[i + 2].Text)
+                {
+                    kazananVarMi = true;
+                }
+            }
+            for (int i = 0; i < 3; i++) //SÜTUN KONTROLU
+            {
+                if (butonlar[i].Text != "" && butonlar[i].Text == butonlar[i + 3].Text && butonlar[i].Text == butonlar[i + 6].Text)
+                {
+                    kazananVarMi = true;
+                }
+            }
+            if (btn1.Text != "" && btn1.Text == btn5.Text && btn1.Text == btn9.Text) //ÇAPRAZ KONTROL
+            {
+                kazananVarMi = true;
+            }
+            else if (btn3.Text != "" && btn3.Text == btn5.Text && btn3.Text == btn7.Text) //ÇAPRAZ KONTROL
+            {
+                kazananVarMi = true;
+            }
+            return kazananVarMi;
+        }
+
+        public void YeniOyun()
+        {
+            OyunuTemizle();
+            lblBirinciOyuncuKazanmaSayisi.Text = "0";
+            lblIkinciOyuncuKazanmaSayisi.Text = "0";
+            birinciOyuncununKazanmaSayisi = 0;
+            ikinciOyuncununKazanmaSayisi = 0;
+        }
+
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            YeniOyun();
+        }
+
+        #region Butonlarýn clickleri
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            BasilanButon(btn9);
+        }
+        #endregion
     }
 }
